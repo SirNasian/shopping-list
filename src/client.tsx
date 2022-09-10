365d431f9ace76733e96790cf4c36d680c8f8161
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2022-09-10 10:59:36 +0800

    Add prettier to project

--- src/client.tsx
+++ src/client.tsx
@@ -8,37 +8,45 @@ import IconButton from "@mui/material/IconButton";
 import Paper from "@mui/material/Paper";
 
 const App = () => {
-	const [theme, setTheme] = React.useState<Theme>(createTheme({ palette: { mode: "light" }}));
+	const [theme, setTheme] = React.useState<Theme>(
+		createTheme({ palette: { mode: "light" } })
+	);
 
 	const toggleTheme = () => {
 		setTheme(
 			createTheme({
 				palette: {
 					mode: theme.palette.mode === "light" ? "dark" : "light",
-				}
+				},
 			})
 		);
 	};
 
 	return (
 		<ThemeProvider theme={theme}>
-			<Box sx={{
-				alignItems: "center",
-				background: theme.palette.background.default,
-				display: "flex",
-				height: "100%",
-				justifyContent: "center"
-			}}>
+			<Box
+				sx={{
+					alignItems: "center",
+					background: theme.palette.background.default,
+					display: "flex",
+					height: "100%",
+					justifyContent: "center",
+				}}
+			>
 				<Paper>
-					<Box sx={{margin: "12rem"}}>
+					<Box sx={{ margin: "12rem" }}>
 						<IconButton onClick={toggleTheme}>
-							{theme.palette.mode === "light" ? <LightModeIcon /> : <DarkModeIcon />}
+							{theme.palette.mode === "light" ? (
+								<LightModeIcon />
+							) : (
+								<DarkModeIcon />
+							)}
 						</IconButton>
 					</Box>
 				</Paper>
 			</Box>
 		</ThemeProvider>
 	);
-}
+};
 
 createRoot(document.getElementById("root")).render(<App />);
